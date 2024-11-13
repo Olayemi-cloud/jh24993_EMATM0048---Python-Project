871f440f3eb95585d9a278eb5c16059257feebad
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-13 13:13:48 +0100

    Delete fish.py

--- fish.py
+++
@@ -1,8 +0,0 @@
-# Fish class 
-class Fish:
-    def __init__(self, name, demand, sell_price):
-        self.name = name
-        self.demand = demand
-        self.sell_price = sell_price
-        
-
