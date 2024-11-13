32fc6d1f7b6e08d5bac31d6da9ef7a0e28ee3dbf
Olayemi-cloud <jh24993@bristol.ac.uk>
2024-11-13 09:00:53 +0100

    Q1 Modules

---
+++ fish.py
@@ -0,0 +1,8 @@
+# Fish class 
+class Fish:
+    def __init__(self, name, demand, sell_price):
+        self.name = name
+        self.demand = demand
+        self.sell_price = sell_price
+        
+
