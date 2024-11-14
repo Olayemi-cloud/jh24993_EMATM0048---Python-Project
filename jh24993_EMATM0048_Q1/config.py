7a0bee776069dda5af587d2b50fc714812d94341
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-14 13:44:26 +0100

    Details on ware house saved as config

---
+++ jh24993_EMATM0048_Q1/config.py
@@ -0,0 +1,15 @@
+# Configuration settings 
+
+class Config:
+    WAREHOUSE_DEPRECIATION = True
+    TECHNICIAN_WEEKLY_RATE = 500
+    APPLY_DEPRECIATION = True 
+    INITIAL_CASH = 10000
+    RENT_UTILITIES_COST = 1500
+    WAREHOUSE_CAPACITY = {
+        "fertiliser": 20,
+        "feed": 400,
+        "salt": 200
+    }
+    
+
