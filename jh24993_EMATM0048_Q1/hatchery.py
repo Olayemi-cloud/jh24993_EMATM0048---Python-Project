0753cbba53f433d9bde672ae0f9c2184330f50b8
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-14 13:46:38 +0100

    Building the hatchery class starting with fish supply

--- jh24993_EMATM0048_Q1/hatchery.py
+++ jh24993_EMATM0048_Q1/hatchery.py
@@ -1,4 +1,29 @@
-import Technician as tc
-import warehouse as wh
+# models/hatchery.py
 
-class
+from Technician import Technician
+from fish import Fish
+from warehouse import Warehouse
+from config import Config
+
+class Hatchery:
+    def __init__(self):
+        self.cash = Config.INITIAL_CASH
+        self.technicians = []
+        self.fish_types = [
+            Fish("Clef Fins", 25, 25),
+            Fish("Timpani Snapper", 10, 10),
+            Fish("Andalusian Brim", 15, 15),
+            Fish("Plagal Cod", 20, 20),
+            Fish("Fugue Flounder", 30, 30),
+            Fish("Modal Bass", 50, 50)
+        ]
+        self.warehouses = [
+            Warehouse("Main", Config.WAREHOUSE_CAPACITY),
+            Warehouse("Auxiliary", Config.WAREHOUSE_CAPACITY)
+        ]
+        
+#Add cash function for initial hatchery invest        
+
+    def add_cash(self, amount):
+        self.cash += amount
+        print(f"Added cash: {amount}. New balance: {self.cash}")
