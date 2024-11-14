7c37f0095b9bc9befb80c0515c8db69e26df296b
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-14 19:02:21 +0100

    Main class created

---
+++ jh24993_EMATM0048_Q1/main.py
@@ -0,0 +1,12 @@
+from hatchery import Hatchery
+
+def run_simulation():
+    print("Welcome to the Hatchery Simulation!")
+    
+    # Set up initial conditions
+    hatchery = Hatchery()
+    quarters = int(input("Enter the number of quarters to run the simulation (default is 8 for two years): ") or 8)
+print('Enter number of of quarters: ' + 'quarters')  
+ 
+        
+
