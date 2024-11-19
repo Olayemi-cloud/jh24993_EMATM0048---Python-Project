b95339f48c2a333acdd7b6cd03e9985441c2b695
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-19 16:46:14 +0100

    Updated Warehouse class Please enter the commit message for your changes. Lines starting

--- jh24993_EMATM0048_Q1/warehouse.py
+++ jh24993_EMATM0048_Q1/warehouse.py
@@ -1,11 +1,12 @@
 # In warehouse.py
 class Warehouse:
     def __init__(self, name, capacity):
+        """
+        param name: Name of the warehouse
+        param capacity: Maximum capacity of the warehouse
+        """
         self.name = name
         self.capacity = capacity
-        self.depreciation = 0  # Initial depreciation if needed
+        self.current_stock = 0
 
-    def apply_depreciation(self, depreciation_amount):
-        self.depreciation += depreciation_amount
-        # Adjust other attributes if needed
 
