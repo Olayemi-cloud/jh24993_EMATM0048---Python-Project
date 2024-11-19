3569f3e64d84612525b6135c6e75bf5d78fbacb3
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-19 15:41:34 +0100

    Fish modified to include labour constant etc

--- jh24993_EMATM0048_Q1/fish.py
+++ jh24993_EMATM0048_Q1/fish.py
@@ -1,8 +1,14 @@
-# Fish class 
 class Fish:
-    def __init__(self, name, demand, sell_price):
+    def __init__(self, name, demand, sell_price, labour_constant, required_fertilizer, required_feed, required_salt):
         self.name = name
         self.demand = demand
         self.sell_price = sell_price
+        self.labour_constant = labour_constant
+        self.required_fertilizer = required_fertilizer
+        self.required_feed = required_feed
+        self.required_salt = required_salt
+
+
+
         
 
