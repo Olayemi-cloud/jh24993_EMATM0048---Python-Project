489841ad55ab3b210ac10fba450d2335af90b596
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-25 02:15:20 +0100

    updated fish

--- jh24993_EMATM0048_Q1/fish.py
+++ jh24993_EMATM0048_Q1/fish.py
@@ -1,5 +1,5 @@
 class Fish:
-    def __init__(self, name, demand, sell_price, labour_constant, required_fertilizer, required_feed, required_salt):
+    def __init__(self, name, demand, sell_price, labour_constant, required_fertilizer, required_feed, required_salt, max_demand):
         self.name = name
         self.demand = demand
         self.sell_price = sell_price
@@ -7,10 +7,12 @@ class Fish:
         self.required_fertilizer = required_fertilizer
         self.required_feed = required_feed
         self.required_salt = required_salt
+        self.max_demand = max_demand
 
     def simulate_sales(self):
         # Placeholder for sales simulation logic
         return min(self.demand, self.demand)  # Simulate selling all available demand
+
 
 
         
