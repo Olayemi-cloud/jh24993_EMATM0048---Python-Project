1eaf4330ca79161345dcfef834f29b85f5c866c4
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-27 05:05:57 +0100

    updATED FISH Module

--- jh24993_EMATM0048_Q1/fish.py
+++ jh24993_EMATM0048_Q1/fish.py
@@ -1,5 +1,20 @@
+"""
+Name: Olayemi Amusile
+Description:  A class to represent types of fish and manage its sales and supply requirements.
+
+Date: November 2024
+"""
+
+
 class Fish:
+    """
+    A class to represent a type of fish and manage its sales and supply requirements.
+
+       
+    """
+
     def __init__(self, name, demand, sell_price, labour_constant, required_fertilizer, required_feed, required_salt, max_demand):
+       
         self.name = name
         self.demand = demand
         self.sell_price = sell_price
@@ -10,9 +25,13 @@ class Fish:
         self.max_demand = max_demand
 
     def simulate_sales(self):
-        # Placeholder for sales simulation logic
-        return min(self.demand, self.demand)  # Simulate selling all available demand
-
+        """
+        simulate_sales():
+            Simulates sales of the fish based on its demand and returns the amount sold.
+        Returns:
+            int: The amount of fish sold .
+        """
+        return min(self.demand, self.demand)  
 
 
         
