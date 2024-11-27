9004c804a7b8cc55e32500106dbbebd7670825e1
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-27 04:56:59 +0100

    final fish with docstring

--- jh24993_EMATM0048_Q1/Technician.py
+++ jh24993_EMATM0048_Q1/Technician.py
@@ -1,11 +1,24 @@
- # Technician class 
+"""
+Name: Olayemi Amusile
+Description:   A class that accounts for technicians(name, and weekly rate).
+
+Date: November 2024
+"""
+
+
 class Technician:
+    """
+    A class that accounts for technicians(name, and weekly rate)
+
+       
+    """
     def __init__(self, name, weekly_rate):
         self.name = name
         self.weekly_rate = weekly_rate
-
     def __str__(self):
         return f"Technician {self.name}"
-    
+
     def __repr__(self):
         return f"Technician(name={self.name})"
+    
+    
