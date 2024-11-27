f9b08effddf3d5ae60eebad9368b8b6d33489fac
Olayemi Amusile <jh24993@bristol.ac.uk>
2024-11-27 05:03:45 +0100

    final config module

--- jh24993_EMATM0048_Q1/config.py
+++ jh24993_EMATM0048_Q1/config.py
@@ -1,15 +1,17 @@
-# Configuration settings 
+"""
+Name: Olayemi Amusile
+Description:  This script contains static variable configuration that was utilized across diffrent functions in the simulation script
+
+Date: November 2024
+"""
+
 
 class Config:
-    WAREHOUSE_DEPRECIATION = True
-    TECHNICIAN_WEEKLY_RATE = 500
-    APPLY_DEPRECIATION = True 
-    INITIAL_CASH = 10000
-    RENT_UTILITIES_COST = 1500
-    WAREHOUSE_CAPACITY = {
-        "fertiliser": 20,
-        "feed": 400,
-        "salt": 200
-    }
+    INITIAL_CASH = 10000  # Starting cash for the simulation
+    TECHNICIAN_WEEKLY_RATE = 500  # Weekly rate for a technician
+    WAREHOUSE_CAPACITY = 1000  # Capacity of each warehouse
+    RENT_UTILITIES_COST = 1500  # Rent and utilities cost per quarter
+    TECHNICIAN_LABOUR_CONSTANT = 0.2
+    FISH_TYPES = 0
     
 
